a056b10207326aade108a837e6d2ff46c189f912
rbhbkk278918 <152058970+rbhbkk278918@users.noreply.github.com>
2023-11-27 14:32:04 +1000

    Add files via upload

---
+++ document.addEventListener('DOMContentLoaded', func.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Привет, коала!');
+
+    var answer = prompt('Вы любите коал?', '');
+
+    if (answer !== null) {
+        if (answer.toLowerCase() === 'да') {
+            alert('Это замечательно! Коалы такие милые!');
+        
+      
+        }
+ 
+     }
+});
