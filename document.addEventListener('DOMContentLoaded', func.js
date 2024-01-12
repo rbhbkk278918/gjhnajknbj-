4c0dd212397169f025dfe615c15af1d9e2d7010f
rbhbkk278918 <152058970+rbhbkk278918@users.noreply.github.com>
2024-01-12 16:05:54 +1000

    Update document.addEventListener('DOMContentLoaded', func.js

--- document.addEventListener('DOMContentLoaded', func.js
+++ document.addEventListener('DOMContentLoaded', func.js
@@ -1,14 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    alert('Привет, коала!');
 
-    var answer = prompt('Вы любите коал?', '');
-
-    if (answer !== null) {
-        if (answer.toLowerCase() === 'да') {
-            alert('Это замечательно! Коалы такие милые!');
-        
-      
-        }
- 
-     }
-});
